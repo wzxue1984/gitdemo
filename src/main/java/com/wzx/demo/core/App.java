cba6723a6bb1154db047b526fb290dd187178cbb
wangzhixue <wangzhixue@medicalunion.cn>
2017-01-17 14:32:35 +0800

    a

--- src/main/java/com/wzx/demo/core/App.java
+++ src/main/java/com/wzx/demo/core/App.java
@@ -4,10 +4,13 @@ package com.wzx.demo.core;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	/**
+	 * 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
