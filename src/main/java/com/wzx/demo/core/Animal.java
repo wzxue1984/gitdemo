db96d7e3ff13e46972971a97f6f772a0972c2371
wzxue1984 <281966650@qq.com>
2017-01-17 14:01:35 +0800

    c

--- src/main/java/com/wzx/demo/core/Animal.java
+++ src/main/java/com/wzx/demo/core/Animal.java
@@ -1,8 +1,15 @@
 package com.wzx.demo.core;
 
 public class Animal {
+	/**
+	 * 
+	 * 
+	 * @param name
+	 * @return
+	 */
 	public String sayHello(String name) {
 		// 加Hello后直接返回入参内容
+		// 新增注释
 		return "Hello " + name;
 	}
 }
