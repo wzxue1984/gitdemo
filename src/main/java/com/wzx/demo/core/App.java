889114d2c05d9acc324df8b27e82cab5436c62c0
wzxue1984 <281966650@qq.com>
2017-01-17 14:26:06 +0800

    b

--- src/main/java/com/wzx/demo/core/App.java
+++ src/main/java/com/wzx/demo/core/App.java
@@ -2,12 +2,10 @@ package com.wzx.demo.core;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
